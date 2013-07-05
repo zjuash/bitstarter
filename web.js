9d2f128cf89a12363b726160e8f36ccc2fb575ac
root <root@ip-10-152-176-170.ec2.internal>
2013-07-05 02:24:43 +0000

    something new

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var fileContent = fs.readFileSync('index.html');
+  var buffer = new Buffer(256);
+  buffer=fileContent;
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
